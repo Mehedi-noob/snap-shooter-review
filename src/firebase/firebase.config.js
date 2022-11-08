247d3fcb55ce622a4f115cfc4ad75c3109d40611
Mehedi Hasan <you@example.com>
2022-11-08 20:09:39 +0600

    basic route created

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0d4yQGvmD5S-cROr9p1vhGgySrDY_EfU",
+  authDomain: "service-review-client-24317.firebaseapp.com",
+  projectId: "service-review-client-24317",
+  storageBucket: "service-review-client-24317.appspot.com",
+  messagingSenderId: "948797869457",
+  appId: "1:948797869457:web:7a8b55dcd12f00321dde30"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
